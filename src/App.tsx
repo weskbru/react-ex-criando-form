e417b6c29465fa749a9e272a05c302cb8dac49cf
wesley bruno <weskbruno@gmail.com>
2022-11-08 00:11:25 -0300

    resolução do exe curso de react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
+import react ,{useState } from "react";
+type ChangeEvent = react.ChangeEvent<HTMLInputElement>
+
+
 const App = () => {
+  const [nome , setNome] = useState('')
+  const [sobrenome , setSobreNome] = useState('')
+  const [idade , setIdade] = useState('')
+
+  const changeName = (event:ChangeEvent) => {
+    setNome(event.target.value)
+  }
+
+  const changeSobreNome = (event: ChangeEvent) => {
+    setSobreNome(event.target.value)
+  }
+
+  const changeIdade = (event: ChangeEvent) => {
+    setIdade(event.target.value)
+  }
+
+
   return (
-    <div>
+    <>
       <div>
         Nome:
-        <input type="text" />
+        <input type="text" value={nome} onChange={changeName}/>
       </div>
 
       <div>
         Sobrenome:
-        <input type="text" />
+        <input type="text" value={sobrenome} onChange={changeSobreNome}/>
       </div>
 
       <div>
         Idade:
-        <input type="text" />
+        <input type="text" value={idade} onChange={changeIdade}/>
       </div>
 
       <hr/>
 
-      Olá ---, tudo bem?<br/>
-      Você tem -- anos.
-    </div>
+      <p>Olá {`${nome} ${sobrenome}`}, tudo bem?<br/>
+      Você tem {idade} anos.
+      </p>
+    </>
   );
 }
 
